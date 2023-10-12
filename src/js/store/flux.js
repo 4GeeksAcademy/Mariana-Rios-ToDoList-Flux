38e270f456923a83ad90261dd2aaf29e9662d9bc
mariana10rios <140272078+mariana10rios@users.noreply.github.com>
2023-10-12 21:09:50 +0000

    completed with flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,99 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
+			todos: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+					done: false,
+					id: "08f9862337094b7a9c5a3506dfbbd126",
+					label: "example task"
 				}
 			]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
+			submitNewtask: (e) => {
+				let store = getStore();
+				let actions = getActions();
+				if (e.key === "Enter") {
+					const newTodo = {label: e.target.value, done: false, id: actions.getNextID()};
+					store.todos.push(newTodo);
+					setStore(store);
+					// setStore({todos:[...store.todos, ...[newTodo]]});
+					console.log(newTodo);
+					actions.uploadTodos(store.todos)
+				};
+				
+			},
+
+			getNextID: () => {
+				let store = getStore();
+				return Math.max.apply(Math, store.todos.map(function(o) { return o.id; })) + 1;
+			},
+
+			hovering: (i) => {
+				document.querySelector("#deletebtn" + i).style.visibility = "visible"
+			},
+
+			notHovering: (i) => {
+				document.querySelector("#deletebtn" + i).style.visibility = "hidden"
+			},
+
+			deleteTask: (i) =>{
+				let store = getStore();
+				let actions = getActions()
+				let newTaskList = store.todos.filter((item, indx) => indx !== i)
+				setStore({todos:newTaskList});
+				if (store.todos.length < 1){
+					alert("dont delete")
+				}
+				else{
+				actions.uploadTodos(newTaskList)
+				}
+			},
+
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			getTodos: () => {
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/mrios')
+					.then(resp => {
+						console.log("is response succesful: " + resp.ok); // will be true if the response is successfull
+						console.log("status code: "+ resp.status); // the status code = 200 or code = 400 etc.
+						return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+					})
+					.then(data => {
+						//here is where your code should start after the fetch finishes
+						console.log(data); //this will print on the console the exact object received from the server
+						setStore({todos: data})
+					})
+					.catch(error => {
+						//error handling
+						console.log(error);
+					});
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			uploadTodos: (todos) => {
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/mrios', {
+					method: "PUT",
+					body: JSON.stringify(todos),
+					headers: {
+					  "Content-Type": "application/json"
+					}
+				  })
+				  .then(resp => {
+					  console.log(resp.ok); // will be true if the response is successfull
+					  console.log(resp.status); // the status code = 200 or code = 400 etc.
+					  console.log(resp.text()); // will try return the exact result as string
+					  return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				  })
+				  .then(data => {
+					  //here is where your code should start after the fetch finishes
+					  console.log(data); //this will print on the console the exact object received from the server
+				  })
+				  .catch(error => {
+					  //error handling
+					  console.log(error);
+				  });
+			},
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import injectContext from "../store/appContext";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container text-center">
+			<div>
+				<h1 className="header">ToDo's</h1>
+			</div>
+			<div>
+				<input type="text" className="py-3 w-100 inputLine" placeholder="What needs to be done?" onKeyDown={(e) => actions.submitNewtask(e)}/>
+			</div>
+			<div>
+				<ul className="tasksUL">
+					{store.todos.map((task, i) => (
+						<li className="listItem" onMouseOver={() => actions.hovering(i)} onMouseOut={() => actions.notHovering(i)} key={i}>
+							{task.label}
+							<i onClick={() => actions.deleteTask(i)} className="fa-solid fa-xmark deletebtn" id={"deletebtn" + i}></i>
+						</li>
+						))}
+				</ul>
+			</div>
+			<div className="itemsLeft">{store.todos.length} Items Left</div>
+		</div>
+	);
+};
+
+export default injectContext(Home);
